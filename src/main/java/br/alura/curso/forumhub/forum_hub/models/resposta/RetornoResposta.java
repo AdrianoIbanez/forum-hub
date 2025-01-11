1c67b39254d901bbbb79c5baa3f65726d8209697
Adriano Ibanez <dricoibanez@gmail.com>
2025-01-11 10:53:14 -0300

    Criando o pacote Resposta.

---
+++ src/main/java/br/alura/curso/forumhub/forum_hub/models/resposta/RetornoResposta.java
@@ -0,0 +1,15 @@
+package br.alura.curso.forumhub.forum_hub.models.resposta;
+
+import java.time.LocalDateTime;
+
+public record RetornoResposta(String titulo_topico,
+                              String mensagem,
+                              String nome,
+                              LocalDateTime horario) {
+    public RetornoResposta(Resposta resposta) {
+        this(resposta.getTopico().getTitulo(),
+                resposta.getMensagem(),
+                resposta.getUsuario().getNome(),
+                resposta.getHorario());
+    }
+}
